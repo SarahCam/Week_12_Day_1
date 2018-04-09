420004129ed33b1cbcd4a586564b462a4d765cb0
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-09 15:40:53 +0100

    Added pseudocode for basic js programmatic way to add more cats to the DOM

---
+++ public/app.js
@@ -0,0 +1,21 @@
+
+
+const app = function(){
+  // 1. Create 'parent' ul and its className (cat)
+
+  // 2. Create 'name' child li and its text: 'Name: xyz'
+
+  // 3. Create 'food' child li and its text: 'Favourite food: abc'
+
+  // 4. Create 'image' child li and its img
+
+  // 5. Append the 'name' to the 'parent' cat
+  // 5. Append the 'food' to the 'parent' cat
+  // 5. Append the 'image' to the 'parent' cat
+
+  // 6. Add the 'parent' cat to the list of cats
+
+  debugger;
+}
+
+window.onload = app;
