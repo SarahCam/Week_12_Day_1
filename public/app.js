f177edfedd6a02f3bb9319e7b893b79fdb5def8b
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-09 20:44:58 +0100

    Finished refactoring - so that parameters are used to create the innerText

--- public/app.js
+++ public/app.js
@@ -6,13 +6,13 @@ const createCat = function(){
 
 const createName = function(name){
   let myName = document.createElement("li");
-  myName.innerText = "Name: Moggy";
+  myName.innerText = "Name: " + name;
   return myName;
 }
 
 const createFood = function(food){
   let myFood = document.createElement("li");
-  myFood.innerText = "Favourite food: Beans";
+  myFood.innerText = "Favourite food: " + food;
   return myFood;
 }
 
@@ -24,7 +24,7 @@ const createPicture = function(){
 const createImage = function(picture){
   let myImage = document.createElement("img");
   myImage.width = "500";
-  myImage.src = "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg";
+  myImage.src = picture;
   return myImage;
 }
 
@@ -48,42 +48,7 @@ const addCat = function(name, food, picture){
 
 const app = function(){
 
-  addCat("Moggy", "Beans", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
-  // // 1. Create parent 'ul' and its className (cat)
-  // let cat = document.createElement("ul");
-  // cat.classList.add("cat");
-  //
-  // // 2. Create 'child 'li' for name and its text: 'Name: xyz'
-  // let name = document.createElement("li");
-  // name.innerText = "Name: Moggy";
-  //
-  // // 3. Create 'child 'li' for food and its text: 'Favourite food: abc'
-  // let food = document.createElement("li");
-  // food.innerText = "Favourite food: Beans"
-  //
-  // // 4. Create child 'li' for picture and its img
-  // let picture = document.createElement("li");
-  //
-  // let image = document.createElement("img");
-  // image.width = "500";
-  // image.src = "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"
-  //
-  // // 5. Append the img 'image' to the 'picture' li
-  // picture.appendChild(image);
-  //
-  // // 6. Append the li 'name' to the 'cat' ul
-  // cat.appendChild(name);
-  //
-  // // 7. Append the li 'food' to the 'cat' ul
-  // cat.appendChild(food);
-  //
-  // // 8. Append the li 'picture' to the 'cat' ul
-  // cat.appendChild(picture);
-  //
-  // // 9. Add the 'ul' cat to the list of cats
-  // let cats = document.querySelector("#cats");
-  // cats.appendChild(cat);
-
+  addCat("Moggy", "Beans", "http://i0.kym-cdn.com/entries/icons/original/000/000/774/lime-cat.jpg");
   // debugger;
 }
 
