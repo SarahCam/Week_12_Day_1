9d239a9f8a23b0600bd3be790b802d80b5c08895
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-09 21:12:51 +0100

    Replaced the hardcoded HTML for Boba, Barnaby and Max with calls to the JS function instead to generate the HTMl in the DOM

--- public/app.js
+++ public/app.js
@@ -47,8 +47,12 @@ const addCat = function(name, food, picture){
 }
 
 const app = function(){
-
-  addCat("Moggy", "Beans", "http://i0.kym-cdn.com/entries/icons/original/000/000/774/lime-cat.jpg");
+  addCat("Boba", "Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
+  addCat("Barnaby", "Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
+  addCat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
+  addCat("Moggy", "Limes", "http://i0.kym-cdn.com/entries/icons/original/000/000/774/lime-cat.jpg");
+  addCat("Smelly", "Beans", "http://i0.kym-cdn.com/photos/images/original/000/793/387/c96.jpg");
+  addCat("Furry", "Birds", "https://upload.wikimedia.org/wikipedia/commons/1/14/Chinchilla_cat_%285833713404%29.jpg");
   // debugger;
 }
 
